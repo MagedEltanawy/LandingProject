698bbaac7bb65b74defad0e92ae00ea39e94f7f7
MagedEltanawy <66372493+MagedEltanawy@users.noreply.github.com>
2021-09-20 01:42:50 +0200

    Add files via upload

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -12,61 +12,7 @@
  * JS Standard: ESlint
  * 
  */
-
-
-
-
-// add sections
-createSection();
-createSection();
-
-/**
- * Define Global Variables
- * 
- */
-const sections = document.querySelectorAll('main section'); //select sections from main 
-const navList = document.querySelector('#navbar__list'); //select the list of navigation parent UL element
-const fragment = document.createDocumentFragment(); //make fragment to decrease reflow and repaint (efficient)
-const main = document.querySelector('main'); //select main element contains sections after the added section
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
- */
-//add the active section class
-function activeSection(section) {
-    const rec = section.getBoundingClientRect();
-    if (rec.top <= 200) {
-        let nodes = section.parentNode.childNodes
-        nodes.forEach(function(node) {
-            if (node.nodeName == 'SECTION') {
-                node.classList.remove('your-active-class');
-            }
-        })
-        section.classList.add('your-active-class');
-    }
-
-    showNavList();
-}
-
-function showNavList() {
-    setTimeout(function(e) {
-        const x = document.body.parentElement.getBoundingClientRect();
-        if (x.top == 0) {
-            document.querySelector('nav').style.display = 'block';
-        }
-    }, 100)
-}
-
-function createNavLinks(section) { //function for list creation
-    // sectionAddListener(section);
-    const listItem = document.createElement('li'); //create empty list element
-    listItem.innerHTML = `<a href='' link='#${section.getAttribute('id')}'>${section.getAttribute('data-nav')}</a>`; //add html contents to list element
-    fragment.appendChild(listItem); //add the links to the fragment 
-}
-
-function createSection() { // function to add more sections to page
+ const createSection = _ => { // function to add more sections to page
     const main = document.querySelector('main'); //select main element contains sections 
     const section = document.createElement('section');
     const div = document.createElement('div')
@@ -87,6 +33,60 @@ function createSection() { // function to add more sections to page
     main.appendChild(section);
 }
 
+
+// add sections
+createSection();
+createSection();
+
+/**
+ * Define Global Variables
+ * 
+ */
+const sections = document.querySelectorAll('main section'); //select sections from main 
+const navList = document.querySelector('#navbar__list'); //select the list of navigation parent UL element
+const fragment = document.createDocumentFragment(); //make fragment to decrease reflow and repaint (efficient)
+const main = document.querySelector('main'); //select main element contains sections after the added section
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+ */
+// function to show the nav list after hide it while scrolling
+const showNavList = _ => {
+    setTimeout(e => {
+        const x = document.body.parentElement.getBoundingClientRect();
+        if (x.top == 0) {
+            document.querySelector('nav').style.display = 'block';
+        }
+    }, 100)
+}
+
+//add the active section class
+const activeSection = section => {
+    const rec = section.getBoundingClientRect();
+    if (rec.top <= 200) {
+        let nodes = section.parentNode.childNodes
+        nodes.forEach(node => {
+            if (node.nodeName == 'SECTION') {
+                node.classList.remove('your-active-class');
+            }
+        })
+        section.classList.add('your-active-class');
+    }
+
+    showNavList();
+}
+
+const createNavLinks = section => { //function for list creation
+    // sectionAddListener(section);
+    const listItem = document.createElement('li'); //create empty list element
+    listItem.innerHTML = `<a href='' link='#${section.getAttribute('id')}'>${section.getAttribute('data-nav')}</a>`; //add html contents to list element
+    fragment.appendChild(listItem); //add the links to the fragment 
+}
+
+
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -96,13 +96,13 @@ function createSection() { // function to add more sections to page
 
 // build the nav 
 sections.forEach(createNavLinks); //add sections to the fragment
-document.addEventListener('scroll', function(e) {
+document.addEventListener('scroll', e => {
     sections.forEach(activeSection);
     document.querySelector('nav').style.display = 'none';
 }) //
 navList.appendChild(fragment); //add the fragment to the nav list
 //Add eventListener to Nav links
-navList.addEventListener('click', function(evnt) {
+navList.addEventListener('click', evnt => {
     if (evnt.target.nodeName === 'A') {
         evnt.preventDefault();
         document.querySelector(evnt.target.getAttribute('link')).scrollIntoView({
@@ -111,7 +111,7 @@ navList.addEventListener('click', function(evnt) {
     }
 
 })
-document.querySelector('button').addEventListener('click', function(e) {
+document.querySelector('button').addEventListener('click', e => {
     document.body.parentElement.scrollIntoView();
     showNavList();
 })
